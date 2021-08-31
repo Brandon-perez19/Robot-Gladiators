6a17b7cf422af0a21e42c6e76be9ea03c921aca6
Brandon <brandonperez@brandons-mbp.lan>
2021-08-31 18:17:04 -0400

    javascript added

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+//this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName)
+
+fight();
