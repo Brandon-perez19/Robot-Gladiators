7a59c9e0e7458184376581d398cc5b74da52112b
Brandon <brandonperez@brandons-mbp.lan>
2021-09-06 21:44:13 -0400

    updated shop feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -146,27 +146,23 @@ var endGame = function() {
 var shop = function() {
   //ask player what they'd like to do 
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
   );
+  shopOptionPrompt = parseInt(shopOptionPrompt);
   switch (shopOptionPrompt){
-    case "refill":
-    case "REFILL":
+    case 1:
       playerInfo.refillHealth();
       break;
-    case "UPGRADE":
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case "LEAVE":
-    case "leave":
+    case 3:
       window.alert("Leaving the store.");
-
       //do nothing, so function will end
     break;
 
     default:
       window.alert("You did not pick a valid option. Try again.");
-
       //call shop() again to force player to pick a valid option
       shop();
     break;
